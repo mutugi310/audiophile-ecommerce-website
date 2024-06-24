bb23239e19abaee2363becb9444260849d31f401
kelvinmutugi210 <kelvinmutugi210@gmail.com>
2024-06-24 19:20:51 +0300

    Lazy load components in app component to optimize site loading.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
+import {Routes, Route} from "react-router-dom"
+import {CartProvider} from './context/CartContext'
+import React, { useState,lazy, Suspense } from 'react'
 import './App.css'
 import Header from './pages/Header'
 import Home from './pages/Home'
-import Headphones from './pages/Headphones'
-import Speakers from './pages/Speakers'
-import Earphones from './pages/Earphones'
-import Cart from './pages/Cart'
 import Footer from './pages/Footer'
-import Checkout from './pages/Checkout'
-import ThankYou from './pages/ThankYou'
-import {Routes, Route} from "react-router-dom"
-import ProductPage from './assets/productPages/ProductPage'
-import {CartProvider} from './context/CartContext'
-import { useState } from 'react'
+
+
+const Headphones = lazy(()=> import('./pages/Headphones'))
+const Speakers = lazy(()=> import('./pages/Speakers'))
+const Earphones = lazy(()=> import('./pages/Earphones'))
+const Cart = lazy(()=> import('./pages/Cart'))
+const Checkout = lazy(()=> import('./pages/Checkout'))
+const ProductPage = lazy(()=> import('./assets/productPages/ProductPage'))
+
 
 
 function App() {
@@ -20,7 +22,6 @@ function App() {
     setIsCartOpen(!isCartOpen)
    }
    
-   
 
   return (
     <>
@@ -28,16 +29,18 @@ function App() {
         <div className=' max-w-screen min-h-screen flex flex-col'>
           <Header handleCartOpen={toggleCart} />
           <main className='flex-grow'>
-            <Routes>
-              <Route path='/'  element={<Home />} />
-              <Route path='headphones' element={<Headphones />} />
-              <Route path="/product/:productName" element={<ProductPage />} />
-              <Route path='speakers' element={<Speakers />} />
-              <Route path='earphones' element={<Earphones />} />
-              <Route path='/cart' element={<Cart />} />
-              <Route path='checkout' element={<Checkout onClose={toggleCart} />} />
-              
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                <Route path='/'  element={<Home />} />
+                <Route path='headphones' element={<Headphones />} />
+                <Route path="/product/:productName" element={<ProductPage />} />
+                <Route path='speakers' element={<Speakers />} />
+                <Route path='earphones' element={<Earphones />} />
+                <Route path='/cart' element={<Cart />} />
+                <Route path='checkout' element={<Checkout onClose={toggleCart} />} />
+                
+              </Routes>
+            </Suspense>
           </main>
           <Footer />
           {isCartOpen && <Cart onClose={toggleCart} />}
