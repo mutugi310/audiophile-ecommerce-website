cfaf285c532a8c171dc88f94b451c67b5f19b4c1
kelvinmutugi210 <kelvinmutugi210@gmail.com>
2024-06-25 00:34:19 +0300

    Update hero section font styling

--- src/assets/homeComponents/HeroSection.jsx
+++ src/assets/homeComponents/HeroSection.jsx
@@ -16,8 +16,8 @@ function HeroSection() {
       <div className='w-full h-full  m-0  bg-pBlack/100  flex align-middle items-center   '>
         <div className='  container  m-auto p-4 flex flex-col gap-5 items-center align-middle lg:items-start lg:px-10 '>
           <span className='text-sm uppercase'>New Product</span>
-          <h1 className='text-4xl font-bold'>XX99 Mark II <br /> Headphones</h1>
-          <p className='text-sm text-center w-1/2 mx-auto lg:text-left lg:ml-0 '>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+          <h1 className='text-5xl font-bold uppercase text-pWhite tracking-widest'>XX99 Mark II <br /> Headphones</h1>
+          <p className='text-sm text-center w-1/2 mx-auto lg:text-left lg:pr-[25%] lg:ml-0 '>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
           <Button to="headphones" variant="cta" className="mt-4">SEE PRODUCT </Button>
           {/* <Link to='headphones' className='bg-pBrown text-white py-2 px-3 inline-block will-change-auto rounded '>Shop Now</Link> */}
         </div>
